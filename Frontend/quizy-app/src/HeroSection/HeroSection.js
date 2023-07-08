10252633349e53bbbb24da948f48b41169486cb2
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-08 15:02:29 +0100

    footer added

--- Frontend/quizy-app/src/HeroSection/HeroSection.js
+++ Frontend/quizy-app/src/HeroSection/HeroSection.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import '../HeroSection/HeroSection.css'
+import './HeroSection.css';
 
 const HeroSection = () => {
   return (
-    <div className="hero-section">
-      <div className="hero-image"></div>
-      <div className="hero-content">
-        <h1>Welcome to the Quiz App</h1>
-        <p>Test your knowledge with our fun quizzes</p>
-        <button className="start-button">Get Started</button>
+    <div className="jumbotron hero-section animate__animated animate__fadeIn">
+      <div className='hero-content'>
+      <h1 className="display-4">Welcome to Quizy App</h1>
+      <p className="lead">Have fun with interesting Quizzes</p>
+      <button className="btn btn-primary btn-lg">Get Started</button>
       </div>
     </div>
   );
 };
 
 export default HeroSection;
+
 
   
--- Frontend/quizy-app/src/components/Home.js
+++ Frontend/quizy-app/src/components/Home.js
@@ -2,18 +2,26 @@ import React from 'react'
 import HeroSection from '../HeroSection/HeroSection';
 import QuizCategory from '../QuizCategory/QuizCategory';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer'
+import { Helmet } from 'react-helmet' 
 
 export const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Quizzy - Home</title>
+      </Helmet>
       <div className='header'>
-          <Header />
+        <Header />
       </div>
       <div className='hero_section'>
         <HeroSection />
       </div>
       <div className='main_category'>
         <QuizCategory />
+      </div>
+      <div className='footer'>
+        <Footer />
       </div>
       </>
   )
