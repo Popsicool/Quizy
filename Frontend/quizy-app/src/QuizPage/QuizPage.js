d87246298a63771e0bc683465292c79071612851
Popsicool <akinolasamson1234@gmail.com>
2023-07-11 10:55:39 +0100

    quiz pages

--- Frontend/quizy-app/src/QuizPage/QuizPage.js
+++ Frontend/quizy-app/src/QuizPage/QuizPage.js
@@ -2,20 +2,40 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 
 export const QuizPage = () => {
+    const [quiz, setQuiz] = useState([])
     const [questions, setQuestions] = useState([])
     const {id} = useParams()
     useEffect(()=>{
-    const url = `https://quizy.popsicool.tech/api/v1/quiz?id=${id}`
+    const url = `http://localhost:8000/api/v1/quiz?id=${id}`
         fetch(url)
         .then(res => {
         return res.json()
         })
         .then((data) => {
-            setQuestions(data)
+            setQuiz(data)
+            setQuestions(data.questions)
         })
         }, []
     )
   return (
-    <div>QuizPage</div>
+    <>
+    {quiz && 
+        <div>{quiz.title} <br/></div>
+    }
+    {questions && questions.map((quest) => (
+        <div key={quest.id}>
+            <p>
+                question : {quest.question}
+            </p>
+            <p>Option A: {quest.A}</p>
+            <p>Option B: {quest.B}</p>
+            <p>Option C: {quest.C}</p>
+            <p>Option D: {quest.D}</p>
+            <p>Answer: {quest.answer}</p>
+            <br/>
+        </div>
+    ))}
+    </>
+
   )
 }
