172b7c762e6c031d17b1c20baeb37e84cd6dfb17
codedjay672 <emmajoel6456@gmail.com>
2023-07-12 19:09:19 +0100

    commit before pulling

--- Frontend/quizy-app/src/Header/Header.js
+++ Frontend/quizy-app/src/Header/Header.js
@@ -10,19 +10,24 @@ function Header() {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand"><img src={logo} alt="header" width="40" className="d-inline-block" /><strong>Quizy App</strong></Link>
-      <div className="collapse navbar-collapse custom-flex" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <li className="nav-item nav-link active"><Link to="/">Home</Link></li>
-          <li className="nav-item nav-link"><Link to="/about">About</Link></li>
-          <li className="nav-item nav-link"><Link to="/help">Help</Link></li>
-          {user ?
-          <li className="nav-item nav-link" onClick={() => {
-            localStorage.removeItem("QuizyUser")
-            window.location.reload()
-          }}>Log Out</li>
-          :
-          <li className="nav-item nav-link"><Link to="/login">Log In</Link></li>}
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand"><img src={logo} alt="header" width="40" className="d-inline-block" /><strong>Quizy App</strong></Link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <li className="nav-item nav-link active"><Link to="/">Home</Link></li>
+            <li className="nav-item nav-link"><Link to="/about">About</Link></li>
+            <li className="nav-item nav-link"><Link to="/help">Help</Link></li>
+            {user ?
+            <li className="nav-item nav-link" onClick={() => {
+              localStorage.removeItem("QuizyUser")
+              window.location.reload()
+            }}>Log Out</li>
+            :
+            <li className="nav-item nav-link"><Link to="/login">Log In</Link></li>}
+          </div>
         </div>
       </div>
     </nav>
