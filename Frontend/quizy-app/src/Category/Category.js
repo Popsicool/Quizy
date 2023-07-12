b5cb1ee2d4e969ea1919655ac9db1ce661e140a1
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-12 13:21:16 +0100

    clean up file structures

--- Frontend/quizy-app/src/Category/Category.js
+++ Frontend/quizy-app/src/Category/Category.js
@@ -1,36 +1,3 @@
-// import React, {useState, useEffect} from 'react'
-// import { useParams } from 'react-router-dom'
-// import { Link } from 'react-router-dom'
-
-// export const Category = () => {
-//     const [quizzes, setQuizzes] = useState([])
-//     const {name} = useParams()
-//     useEffect(()=>{
-//     const url = `https://quizy.popsicool.tech/api/v1/get_quiz?category=${name}`
-//         fetch(url)
-//         .then(res => {
-//         return res.json()
-//         })
-//         .then((data) => {
-//             setQuizzes(data)
-//         })
-//         }, []
-//     )
-//   return (
-//     <>
-//     <div>Select a quiz from the {name} Category</div>
-//     {quizzes && quizzes.map((quiz) => (
-//         <div key={quiz.id}>
-//             id of {quiz.id}
-//             {quiz.title} created on {new Date(quiz.created).toString()}
-//             <Link to={`/quiz/${quiz.id}`} className="btn btn-primary">Try it Out</Link>
-//         </div>
-
-//     ))
-//     }
-//     </>
-//   )
-// }
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -72,18 +39,6 @@ export const Category = () => {
           <h1>Welcome to the {name} Quiz Category</h1>
           <hr  />
       </div>
-      {/* <div className='row row-cols-3 mt-4 mb-4 d-flex justify-content-center'>
-        {categories && categories.map((cat) => (
-          <Link to={`/category/${cat.name}`} key={cat.id} className="card m-2" style={{ width: '20rem' }}>
-            <img src={catImages[Math.floor(Math.random() * catImages.length)]} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{cat.name}</h5>
-              <p className="card-text">Test your knowledge in {cat.name}.</p>
-            </div>
-          </Link>
-        ))}
-      </div> */}
-
       <div className=' fw-bold m-4'><h2 className='text-muted mt-4 text-center'>Select a quiz and get started</h2></div>
         <div className='row row-cols-3 mt-4 mb-4 d-flex justify-content-center'>
           {quizzes.map((quiz, index) => (
