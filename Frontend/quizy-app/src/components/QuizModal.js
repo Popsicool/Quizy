5adcce01f0eea14469cbe7d52be13be941de8ec2
Bennet-Ukoh <bennetukoh@gmail.com>
2023-07-13 04:06:18 +0100

    updated the quizmodal to display different messages with grades

--- Frontend/quizy-app/src/components/QuizModal.js
+++ Frontend/quizy-app/src/components/QuizModal.js
@@ -2,17 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const QuizModal = ({ grade }) => {
+  let message;
+  if (grade === 90) {
+    message = 'Congratulations! You are truly a genius.';
+  if (grade >= 90) {
+    message = 'Congratulations! You scored an excellent grade.';
+  } else if (grade >= 80) {
+    message = 'Great job! You scored a very good grade.';
+  } else if (grade >= 70) {
+    message = 'Well done! You scored a good grade.';
+  } else if (grade >= 60) {
+    message = 'Good effort! You scored an average grade.';
+  } else {
+    message = 'Keep practicing! You scored a below-average grade.';
+  }
+
   return (
     <div className='modal-container'>
       <div className='mud'>
         <div>
           <h3>You Scored {grade} %</h3>
-          <p>Your Score has been recorded</p>
+          <p>{message}</p>
           <Link to="/">Go Home</Link>
         </div>
       </div>
     </div>
   );
 };
-
+}
 export default QuizModal;
